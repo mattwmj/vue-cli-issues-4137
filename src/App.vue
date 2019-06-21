1231b2818a48c937ae295d89517225f40d293461
WANGMJ <wangminjie@smec-cn.com>
2019-06-21 13:19:45 +0800

    try to keep process.env.NODE_ENV by optimization.minimize: false

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
 </template>
 
 <script>
@@ -12,17 +12,24 @@ export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  mounted() {
+    if (process.env.NODE_ENV !== 'production') {
+      console.log('Development mode. Log some debug information.')
+    } else {
+      console.log('Production mode. Do nothing.')
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
